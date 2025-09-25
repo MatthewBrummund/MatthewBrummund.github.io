84985adf03eb4728adc9cc2a7d83a23cd7d3bc7f
MatthewBrummund <143558807+MatthewBrummund@users.noreply.github.com>
2025-09-24 17:37:51 -0700

    test

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var downloadButton = document.getElementById('download-button');
-    downloadButton.addEventListener('click', function() {
-        // Assuming the resume PDF is in the same directory
-        window.location.href = 'assets/resume.pdf';
-    });
-});
